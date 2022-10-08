786b731b31e8982f129079c30437fb2320ae4c64
Jakub <jmwierzbicki@gmail.com>
2022-10-08 19:52:39 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+require('dotenv').config()
+
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = "localhost";
+const API_SERVICE_URL = process.env.HOST;
+
+// Logging
+app.use(morgan('dev'));
+
+// Proxy endpoints
+app.use('/*', createProxyMiddleware({
+	target: API_SERVICE_URL,
+	changeOrigin: true,
+}));
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+	res.send('This is a proxy service which proxies to Billing and Account APIs.');
+});
+
+
+// Start the Proxy
+app.listen(PORT, HOST, () => {
+	console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
